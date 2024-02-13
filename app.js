17ec722e809ba69e184a7b5f3981459cc6fb44d5
juvisingh <141285456+juvisingh@users.noreply.github.com>
2024-02-12 17:10:15 -0700

    finished all functionality

--- app.js
+++ app.js
@@ -1,25 +1,53 @@
 var eq = ""
-function changeString(num) {
+function changeString(num, complicated = false) {
     try {
-        if (num != 'equal') {
-            if (num == "AC") {
-                eq = ''
-                document.getElementById('result').innerHTML = eq
+        if (complicated == false) {
+            if (num != 'equal') {
+                if (num == "AC") {
+                    eq = ''
+                    document.getElementById('result').innerHTML = eq
+                }
+                else if (num == "BS") {
+                    eq = eq.slice(0, -1);
+                    document.getElementById('result').innerHTML = eq
+                }
+                else {
+                    eq += num
+                    document.getElementById('result').innerHTML = eq
+                }
             }
-            else if (num == "BS") {
-                eq = eq.slice(0, -1);
+            else{
+                var result = eval(eq)
+                eq += " = " + eval(eq)
                 document.getElementById('result').innerHTML = eq
-            }
-            else {
-                eq += num
-                document.getElementById('result').innerHTML = eq
+                eq = result
             }
         }
-        else{
-            var result = eval(eq)
-            eq += " = " + eval(eq)
-            document.getElementById('result').innerHTML = eq
-            eq = result
+        else {
+            if (num == 'sin') {
+                eq = eval(Math.sin(eq))
+                document.getElementById('result').innerHTML = eq
+            }
+            else if (num == 'sqrt') {
+                eq = eval(Math.sqrt(eq))
+                document.getElementById('result').innerHTML = eq
+            }
+            else if (num == 'e') {
+                eq = eval(Math.log(eq))
+                document.getElementById('result').innerHTML = eq
+            }
+            else if (num == 'log') {
+                eq = eval(Math.log(eq, 10))
+                document.getElementById('result').innerHTML = eq
+            }
+            else if (num == 'cos') {
+                eq = eval(Math.cos(eq, 10))
+                document.getElementById('result').innerHTML = eq
+            }
+            else if (num == 'tan') {
+                eq = eval(Math.tan(eq, 10))
+                document.getElementById('result').innerHTML = eq
+            }
         }
     }
     catch (error) {
@@ -27,3 +55,4 @@ function changeString(num) {
         eq = ''
     }
 }
+
